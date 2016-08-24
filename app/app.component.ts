0de9352f000b2bb6e3953fea34ac8f6b6de59c7a
Heinrich van Vught <hein@Heinrichs-MacBook.local>
2016-08-24 11:39:40 +0100

    wip' ' '

---
+++ app/app.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: '<h1>My first Angular 2 Starter App woo hoo! {{1+2}}</h1>'
+})
+export class AppComponent { }
